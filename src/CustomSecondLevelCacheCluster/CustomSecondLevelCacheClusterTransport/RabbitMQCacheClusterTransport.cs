ca94ccb64c6b24e45404ca9e5e495dccb6ec0cf9
Teoni Valois <teonivalois@gmail.com>
2017-02-18 00:52:59 -0300

    Adding fault tolerance to the RabbitMQ L2 cache eviction provider

--- src/CustomSecondLevelCacheCluster/CustomSecondLevelCacheClusterTransport/RabbitMQCacheClusterTransport.cs
+++ src/CustomSecondLevelCacheCluster/CustomSecondLevelCacheClusterTransport/RabbitMQCacheClusterTransport.cs
@@ -25,7 +25,9 @@ namespace CustomSecondLevelCacheClusterTransport
         private string _rabbitMQQueueName;
         private bool _isConsuming;
 
-        private delegate void RabbitMQConsumerDelegate();
+        //Timeout for consumer reconnection
+        private static int millisecondsTimeout = 30000;
+        private delegate void RabbitMQConsumerDelegate(int milisecondsTimeout);
 
         public override void Init(OpenAccessClusterMsgHandler msgHandler, string serverName, string identifier, IOpenAccessClusterTransportLog log)
         {
@@ -35,15 +37,22 @@ namespace CustomSecondLevelCacheClusterTransport
             if (!ConnectToRabbitMQ())
                 throw new Exception("Failed to connect to RabbitMQ");
 
-            StartConsumingFromRabbitMQ();
+            StartConsumingFromRabbitMQ(millisecondsTimeout);
         }
 
         public override void SendMessage(byte[] buffer)
         {
-            IBasicProperties basicProperties = _rabbitMQModel.CreateBasicProperties();
-            basicProperties.Persistent = true;
+            try
+            {
+                IBasicProperties basicProperties = _rabbitMQModel.CreateBasicProperties();
+                basicProperties.Persistent = true;
 
-            _rabbitMQModel.BasicPublish(this.Localpath, _localRoutingKey, basicProperties, buffer);
+                _rabbitMQModel.BasicPublish(this.Localpath, _localRoutingKey, basicProperties, buffer);
+            }
+            catch (Exception e)
+            {
+                log.LogWarning("Publisher got exception {0}", e.Message);
+            }
         }
 
         public override void Close()
@@ -52,10 +61,7 @@ namespace CustomSecondLevelCacheClusterTransport
             {
                 closed = true;
 
-                _isConsuming = false;
-
-                _rabbitMQConnection?.Close();
-                _rabbitMQModel?.Abort();
+                DisconnectFromRabbitMQ();
             }
             handler = null;
         }
@@ -68,8 +74,7 @@ namespace CustomSecondLevelCacheClusterTransport
             {
                 Close();
 
-                _rabbitMQConnection?.Dispose();
-                _rabbitMQModel?.Dispose();
+                DisconnectFromRabbitMQ();
             }
         }
 
@@ -80,9 +85,11 @@ namespace CustomSecondLevelCacheClusterTransport
         {
             try
             {
+                DisconnectFromRabbitMQ();
+
                 string hostName = string.IsNullOrEmpty(this.multicastAddress) ? "localhost" : this.multicastAddress;
                 string virtualHost = ConnectionFactory.DefaultVHost;
-                if(hostName.Contains("/"))
+                if (hostName.Contains("/"))
                 {
                     virtualHost = hostName.Substring(hostName.IndexOf("/"));
                     hostName = hostName.Substring(0, hostName.IndexOf("/"));
@@ -103,6 +110,12 @@ namespace CustomSecondLevelCacheClusterTransport
 
                 _rabbitMQModel.ExchangeDeclare(this.Localpath, "fanout", durable);
 
+
+                _rabbitMQModel.BasicQos(0, 10, false);
+
+                _rabbitMQQueueName = _rabbitMQModel.QueueDeclare();
+                _rabbitMQModel.QueueBind(_rabbitMQQueueName, this.Localpath, _localRoutingKey);
+
                 return true;
             }
             catch (BrokerUnreachableException e)
@@ -111,18 +124,14 @@ namespace CustomSecondLevelCacheClusterTransport
             }
         }
 
-        private void StartConsumingFromRabbitMQ()
+        private void StartConsumingFromRabbitMQ(int millisecondsTimeout)
         {
-            _rabbitMQModel.BasicQos(0, 1, false);
-
-            _rabbitMQQueueName = _rabbitMQModel.QueueDeclare();
-            _rabbitMQModel.QueueBind(_rabbitMQQueueName, this.Localpath, _localRoutingKey);
             _isConsuming = true;
 
-            new RabbitMQConsumerDelegate(ConsumeMessages).BeginInvoke(null, null);
+            new RabbitMQConsumerDelegate(ConsumeMessages).BeginInvoke(millisecondsTimeout, null, null);
         }
 
-        private void ConsumeMessages()
+        private void ConsumeMessages(int millisecondsTimeout)
         {
             bool autoAck = false;
 
@@ -131,22 +140,68 @@ namespace CustomSecondLevelCacheClusterTransport
 
             while (_isConsuming)
             {
-                BasicDeliverEventArgs e = _rabbitMQSubscription.Next();
-                byte[] body = e.Body;
-                var bodyLength = body.Length;
-
-                if (!_localRoutingKey.Equals(e.RoutingKey))
+                try
                 {
-                    handler.HandleMessage(new MemoryStream(body, 0, bodyLength, false));
-                    log.LogInformation("Received {0} bytes", bodyLength);
-                }
-                else
-                {
-                    log.LogInformation("Got my own eviction");
-                }
+                    BasicDeliverEventArgs e = null;
+                    if (_rabbitMQSubscription.Next(millisecondsTimeout, out e))
+                    {
+                        byte[] body = e.Body;
+                        var bodyLength = body.Length;
 
-                _rabbitMQSubscription.Ack(e);
+                        if (!_localRoutingKey.Equals(e.RoutingKey))
+                        {
+                            handler.HandleMessage(new MemoryStream(body, 0, bodyLength, false));
+                            log.LogInformation("Received {0} bytes", bodyLength);
+                        }
+                        else
+                        {
+                            log.LogInformation("Got my own eviction");
+                        }
+                        _rabbitMQSubscription.Ack(e);
+                    }
+                    else
+                    {
+                        throw new TimeoutException("Receiver timed out");
+                    }
+                }
+                catch (Exception e)
+                {
+                    log.LogWarning("Receiver got exception {0}", e.Message);
+
+                    //Renews the subscription
+                    if (ConnectToRabbitMQ())
+                        StartConsumingFromRabbitMQ(millisecondsTimeout);
+
+                    break;
+                }
             }
+        }
+
+        private void DisconnectFromRabbitMQ()
+        {
+            _isConsuming = false;
+            try
+            {
+                if (_rabbitMQSubscription != null)
+                    _rabbitMQSubscription.Close();
+            }
+            catch { }
+
+            try
+            {
+                if (_rabbitMQConnection != null)
+                    _rabbitMQConnection.Close();
+            }
+            catch { }
+
+            try
+            {
+                if (_rabbitMQModel != null)
+                    _rabbitMQModel.Abort();
+            }
+            catch { }
+
+            _rabbitMQQueueName = null;
         }
         #endregion
     }
